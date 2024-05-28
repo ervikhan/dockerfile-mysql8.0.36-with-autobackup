55345d91d34264f1a281349c3cae24dd750fb6f8
Muhammad Ibnu Ervikhan <m.ervikhan@gmail.com>
2024-05-28 10:34:50 +0700

    Dockerfile using ubuntu 18.04 with mysql5.7 and autobackupmysql using crontab

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#AUTO START WHEN CONTAINER RUNNING
+service mysql start
+service cron start
+
+#CREATE USER MYSQL
+mysql -u root -e "CREATE USER 'EXAMPLE_USER'@'%' IDENTIFIED BY 'EXAMPLE_PASSWD'; GRANT ALL PRIVILEGES ON *.* TO 'EXAMPLE_USER'@'%'; FLUSH PRIVILEGES;"
+
+#RUN SHELL
+/bin/bash
